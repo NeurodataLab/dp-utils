bcdf03d096bf3c09d6db160065d242c22f94de92
Leonid Shtanko <l.shtanko@neurodatalab.com>
2018-11-27 14:43:17 +0300

    new additions

--- routines/logging_routines.py
+++ routines/logging_routines.py
@@ -2,11 +2,25 @@ from functools import wraps
 
 
 def log_args_into(logger):
+
     def logger_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            logger.info('Passed args: {}'.format(args))
-            logger.info('Passed kwargs: {}'.format(kwargs))
+            logger.debug('Passed args in {}: {}'.format(func.__name__, args))
+            logger.debug('Passed kwargs in {}: {}'.format(func.__name__, kwargs))
             return func(*args, **kwargs)
         return wrapper
     return logger_decorator
+
+
+def log_outputs_into(logger):
+
+    def logger_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            ret_val = func(*args, **kwargs)
+            logger.debug('Computed outputs in {}: {}'.format(func.__name__, ret_val))
+            return ret_val
+        return wrapper
+    return logger_decorator
+
