fecd333a3e6acaf53c66ce18bf8f77ded9d417ea
Штанько Леонид Александрович <leonshting@gmail.com>
2018-12-05 01:02:02 +0300

    small fucking fixes

--- data_iterators/iterators/multiprocess_iterator.py
+++ data_iterators/iterators/multiprocess_iterator.py
@@ -36,11 +36,14 @@ class MultiProcessIterator(BaseIterator):
             while True:
                 result = {}
                 idx, data_pack = task_queue.get()  # waiting for available task
-                for key, processor in self._preprocessors.items():
-                    key = key if isinstance(key, tuple) else (key,)
-                    instance = {k: self._joint_storage[k][idx] for k in key}
-                    result.update(processor.process(**instance))
-                result_queue.put(result)
+                try:
+                    for key, processor in self._preprocessors.items():
+                        key = key if isinstance(key, tuple) else (key,)
+                        instance = {k: self._joint_storage[k][idx] for k in key}
+                        result.update(processor.process(**instance))
+                    result_queue.put(result)
+                except (IndexError, IOError) as _:
+                    logger.info('Probably no data for {}'.format(idx))
 
         return task_func
 
@@ -64,23 +67,19 @@ class MultiProcessIterator(BaseIterator):
         if self._num_batches is not None and self._num_batches == self._batch_counter:
             raise StopIteration
 
-        data_packs = [[] for _ in self._data_keys]
-        label_packs = [[] for _ in self._label_keys]
         indices_to_ret = []
 
         sample_num = 0
-
+        data_packs = {}
         while sample_num < self._batch_size:
             try:
-                idx, data, labels = self._output_storage.get(True, 10)
-                # idx - sample index, data - (num_data, ...), labels - (num_labels, ...)
+                idx, data_dict = self._output_storage.get(True, 10)
+                # idx - sample index, data_dict - {'name': data, ...}
                 # if no exception here
-                indices_to_ret.append(idx)
-                for num_data, d in enumerate(data):
-                    data_packs[num_data].append(d)
-                for num_label, l in enumerate(labels):
-                    label_packs[num_label].append(l)
                 sample_num += 1
+                indices_to_ret.append(idx)
+                for key, data in data_dict.items():
+                    data_packs[key].append(data)
             except Empty:
                 break
 
@@ -88,13 +87,6 @@ class MultiProcessIterator(BaseIterator):
         if not self._continue and len(indices_to_ret) == 0:
             raise StopIteration
 
-        data_batched = [mx.nd.array(data_pack) for data_pack in data_packs]
-        labels_batched = [mx.nd.array(label_pack) for label_pack in label_packs]
-
-        self._batch_counter += 1
-        if not self._return_indices:
-            return mx.io.DataBatch(data=data_batched, label=labels_batched, pad=0)
-        else:
-            return mx.io.DataBatch(data=data_batched, label=labels_batched, pad=0), indices_to_ret
+        return self._pack_to_backend(data_packs, indices_to_ret)
 
 
